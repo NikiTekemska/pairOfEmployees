616cc4425c8342edf710a123a5aeb59c7ea76f4f
Nikoleta Tekemska <nikoletatt94@gmail.com>
2023-12-20 23:00:13 +0200

    Update Employee.java
    adding getters and setters to class Employee

--- src/main/java/com/sirmafinal/pairofemployees/Employee.java
+++ src/main/java/com/sirmafinal/pairofemployees/Employee.java
@@ -3,15 +3,47 @@ package com.sirmafinal.pairofemployees;
 import java.time.LocalDate;
 
 class Employee {
-    int empID;
-    int projectID;
-    LocalDate dateFrom;
-    LocalDate dateTo;
+   private int empID;
+   private int projectID;
+   private LocalDate startDate;
+   private LocalDate endDate;
+
+    public int getEmpID() {
+        return empID;
+    }
+
+    public void setEmpID(int empID) {
+        this.empID = empID;
+    }
+
+    public int getProjectID() {
+        return projectID;
+    }
+
+    public void setProjectID(int projectID) {
+        this.projectID = projectID;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate dateFrom) {
+        this.startDate = dateFrom;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate dateTo) {
+        this.endDate = dateTo;
+    }
 
     public Employee(int empID, int projectID, LocalDate dateFrom, LocalDate dateTo) {
         this.empID = empID;
         this.projectID = projectID;
-        this.dateFrom = dateFrom;
-        this.dateTo = dateTo;
+        this.startDate = dateFrom;
+        this.endDate = dateTo;
     }
 }
