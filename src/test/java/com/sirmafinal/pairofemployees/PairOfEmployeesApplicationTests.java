48b74fd57266192bfe86cd1ab31001b450728620
Nikoleta Tekemska <nikoletatt94@gmail.com>
2023-12-20 19:57:02 +0200

    Create PairOfEmployeesApplicationTests.java

---
+++ src/test/java/com/sirmafinal/pairofemployees/PairOfEmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirmafinal.pairofemployees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PairOfEmployeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
