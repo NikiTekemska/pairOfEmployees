e99cd356f5f5fd2a879d72fdc26c9d7ffd21db20
Nikoleta Tekemska <nikoletatt94@gmail.com>
2023-12-24 03:55:51 +0200

    Update PairOfEmployeesApplication.java
    main file to run the whole app

--- src/main/java/com/sirmafinal/pairofemployees/PairOfEmployeesApplication.java
+++ src/main/java/com/sirmafinal/pairofemployees/PairOfEmployeesApplication.java
@@ -1,13 +1,21 @@
 package com.sirmafinal.pairofemployees;
 
+import com.sirmafinal.pairofemployees.model.CommonProjects;
+import com.sirmafinal.pairofemployees.model.Employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
-public class PairOfEmployeesApplication {
+public class
+PairOfEmployeesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PairOfEmployeesApplication.class, args);
-	}
 
+	}
 }
