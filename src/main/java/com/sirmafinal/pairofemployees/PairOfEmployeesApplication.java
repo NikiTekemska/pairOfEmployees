52e69ca2d30df1827b0669c4d1fffed0d1f15acb
Nikoleta Tekemska <nikoletatt94@gmail.com>
2023-12-24 04:57:57 +0200

    Update PairOfEmployeesApplication.java

--- src/main/java/com/sirmafinal/pairofemployees/PairOfEmployeesApplication.java
+++ src/main/java/com/sirmafinal/pairofemployees/PairOfEmployeesApplication.java
@@ -1,14 +1,7 @@
 package com.sirmafinal.pairofemployees;
 
-import com.sirmafinal.pairofemployees.model.CommonProjects;
-import com.sirmafinal.pairofemployees.model.Employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class
