259764cc1d6ade50e37576f86462b2946682b122
Nikoleta Tekemska <nikoletatt94@gmail.com>
2023-12-24 03:52:02 +0200

    Update CSVReader.java

--- src/main/java/com/sirmafinal/pairofemployees/CSVReader.java
+++ src/main/java/com/sirmafinal/pairofemployees/CSVReader.java
@@ -1,13 +1,18 @@
 package com.sirmafinal.pairofemployees;
 
+import com.sirmafinal.pairofemployees.model.Employee;
+import com.sirmafinal.pairofemployees.service.EmployeeService;
+import org.springframework.stereotype.Component;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-
+@Component
 public class CSVReader implements DataReader {
+    private EmployeeService employeeService;
     @Override
     public List<Employee> readData(String filename) {
         List<Employee> employees = new ArrayList<>();
@@ -15,13 +20,13 @@ public class CSVReader implements DataReader {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] data = line.split(", ");
-                int empID = Integer.parseInt(data[0]);
-                int projectID = Integer.parseInt(data[1]);
+                int empId = Integer.parseInt(data[0]);
+                int projectId = Integer.parseInt(data[1]);
 
                 LocalDate dateFrom = LocalDate.parse(data[2]);//parseDate(data[2]);
                 LocalDate dateTo = (data[3].equalsIgnoreCase("NULL")) ? LocalDate.now() : LocalDate.parse(data[3]); //parseDate(data[3]);
 
-                employees.add(new Employee(empID, projectID, dateFrom, dateTo));
+                employees.add(new Employee(empId, projectId, dateFrom, dateTo));
             }
         } catch (IOException ex) {
             ex.printStackTrace();
